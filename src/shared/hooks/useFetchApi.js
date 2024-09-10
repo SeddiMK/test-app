166801087cb6ce971aba1ccbb2ede5f6caa1d3d1
SeddiMK <web.egorovm@gmail.com>
2024-09-10 15:44:10 +0300

    fetch_!

---
+++ src/shared/hooks/useFetchApi.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export const useFetchApi = reference => {
+	const url = process.env.REACT_APP_URL || ''
+	const token = process.env.REACT_APP_AMOCRM_LONG_TERM_TOKEN || ''
+	const [dataApi, setDataApi] = useState([])
+
+	console.log(dataApi, 'dataApi-----------')
+
+	const getData = async () => {
+		console.log(reference.current, 'reference-----------')
+
+		const config = {
+			method: 'GET',
+			mode: 'no-cors',
+			headers: { Authorization: 'Bearer ' + token },
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': true,
+			'Access-Control-Allow-Headers':
+				'Content-Type, Authorization, X-Requested-With',
+		}
+		// const headers = {
+		// 	Authorization: 'Bearer ' + token,
+		// }
+
+		// const response = await fetch(url + 'leads', {
+		// 	mode: 'no-cors',headers,
+		// }).then(response => response.json())
+
+		console.log(config, 'config-----------')
+
+		const response = await fetch(
+			'https://egorow198704.amocrm.ru/api/v4/leads',
+			config
+		).then(response => response.json())
+
+		// const response = await axios.create({
+		// 	baseURL: 'https://egorow198704.amocrm.ru/api/v4/leads',
+		// 	timeout: 333,
+		// 	mode: 'no-cors',
+		// 	headers: { Authorization: `Bearer ${token}` },
+		// })
+
+		setDataApi(response)
+	}
+
+	useEffect(() => {
+		getData()
+	}, [])
+
+	// можно сделать чтобы хук возвращал весь объект
+	useEffect(() => {
+		// dataApi
+		console.log(dataApi, 'dataApi useEffect-----------')
+	}, [])
+
+	return {
+		dataApi,
+		// name,
+		// price,
+		// id,
+	}
+}
---
+++ src/shared/hooks/useToggle.js
@@ -0,0 +1,12 @@
+//Using useToggle Hook
+import { useState } from 'react'
+
+export const useToggle = initialState => {
+	const [toggleValue, setToggleValue] = useState(initialState)
+
+	const toggler = () => {
+		setToggleValue(!toggleValue)
+	}
+
+	return [toggleValue, toggler]
+}
